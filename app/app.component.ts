ceba32a7275ec7a9fc9c348ae45e8f22d6ab9548
manish <manish.kapoor@tothenew.com>
2017-05-07 01:09:01 +0530

    Added Courses Component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,10 @@
 import {Component} from 'angular2/core';
+import {CoursesComponent} from './courses.component'
+
 
 @Component({
     selector: 'my-app',
-    template: '<h1>Hello angular</h1>'
+    template: '<h1>Hello angular</h1><courses></courses>',
+    directives: [CoursesComponent]
 })
 export class AppComponent { }
