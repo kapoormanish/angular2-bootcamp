da02c54ea710ba4f74e812ac15bfb80b20cca6b8
manish <manish.kapoor@tothenew.com>
2017-05-08 00:04:06 +0530

    added componint with service injection

--- app/courses.component.ts
+++ app/courses.component.ts
@@ -1,9 +1,22 @@
 import {Component} from 'angular2/core'
-
+import {CourseService} from './course.service'
 @Component({
     selector:  'courses',
-    template: '<h2>Courses</h2>'
+    template: `<h2>{{ title }}</h2>
+    <ul>
+        <li *ngFor="#course of courses">
+            {{ course }}
+        </li>
+    <ul>
+    `,
+    providers: [CourseService]
 })
 export class CoursesComponent{
+    title: string =  "Courses";
+    courses=["Course6", "Course5", "Course4"];
 
+    constructor(courseService: CourseService){
+        this.courses = courseService.getCourses();
+    }
+    
 }
