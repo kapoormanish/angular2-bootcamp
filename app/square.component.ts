53f4a64fe4b875d6d9e5d983678e497707abc6b7
manish <manish.kapoor@tothenew.com>
2017-05-10 01:00:31 +0530

    Moved content of star component to separate html file.

--- app/square.component.ts
+++ app/square.component.ts
@@ -2,20 +2,7 @@ import {Component, EventEmitter, Input, Output} from "angular2/core";
 
 @Component({
     selector:'star',
-    template:`
-        <style type="text/css">
-            .blue{
-                background-color: blue;
-            }
-            .active{
-                background-color: red;
-            }
-            
-        </style>
-        <br>
-        <div (click)="toggleState()" height="100px" width="100px" class="blue" [class.active]="state"  >&nbsp;</div>
-        <br>
-    `
+    templateUrl: "app/star.template.html"
 })
 export class SquareComponent{
     @Input()
@@ -28,5 +15,4 @@ export class SquareComponent{
         this.state = !this.state;
         this.change.emit({state: this.state})
     }
-
 }
