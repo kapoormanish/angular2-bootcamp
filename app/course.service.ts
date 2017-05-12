2241355178c8376ceb83ba4c6f3ebb6f578a0460
manish <manish.kapoor@tothenew.com>
2017-05-12 12:58:43 +0530

    *ngIf

--- app/course.service.ts
+++ app/course.service.ts
@@ -4,6 +4,6 @@ import {Injectable} from 'angular2/core';
 @Injectable()
 export class CourseService{
     getCourses(): string[] {
-        return ["Course1", "Course2", "Course3"];
+        return [];
     }
 }
--- app/courses.component.ts
+++ app/courses.component.ts
@@ -2,21 +2,26 @@ import {Component} from 'angular2/core'
 import {CourseService} from './course.service'
 @Component({
     selector:  'courses',
-    template: `<h2>{{ title }}</h2>
-    <ul>
-        <li *ngFor="#course of courses">
-            {{ course }}
-        </li>
-    <ul>
+    template: `
+    <h2>{{ title }}</h2>
+    <div *ngIf="courses.length > 0">
+        <ul>
+            <li *ngFor="#course of courses">
+                {{ course }}
+            </li>
+        </ul>
+    </div>
+    <div *ngIf="courses.length == 0">
+        You dont have any courses yet.
+    </div>
     `,
     providers: [CourseService]
 })
 export class CoursesComponent{
     title: string =  "Courses";
-    courses=["Course6", "Course5", "Course4"];
+    courses=[];
 
     constructor(courseService: CourseService){
         this.courses = courseService.getCourses();
     }
-    
 }
